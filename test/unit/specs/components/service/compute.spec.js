853b8baff771cf76b8b23e787501f47a07ad5656
Dustin Zhang <dike.zhang@gmail.com>
2018-05-11 14:16:14 -0400

    clean test for compute.js

--- test/unit/specs/components/service/compute.spec.js
+++ test/unit/specs/components/service/compute.spec.js
@@ -11,35 +11,30 @@ describe('compute.js module', () => {
 
   it('function initAdds create array', () => {
     const add_list = compute.initAdds(0, 3)
-    // console.log('k=%j', add_list)
     expect(add_list.length).toEqual(16)
   })
 
   it('function initSubstracts create array', () => {
     const add_list = compute.initAdds(0, 3)
     const sub_list = compute.initSubstracts(add_list)
-    // console.log('k=%j', sub_list)
     expect(sub_list.length).toEqual(16)
   })
 
   it('function initMutiplies create array', () => {
     const multiply_list = compute.initMutiplies(0, 3)
-    // const sub_list = compute.initSubstracts(add_list)
-    // console.log('k=%j', sub_list)
     expect(multiply_list.length).toEqual(16)
   })
 
   it('function initDivides create array', () => {
     const multiply_list = compute.initMutiplies(0, 3)
     const divide_list = compute.initDivides(multiply_list)
-    // console.log('k=%j', divide_list)
+
     expect(divide_list.length).toEqual(12)
   })
 
   it('function pick create array', () => {
     const multiply_list = compute.initMutiplies(0, 3)
     const result = compute.pick(multiply_list, 5)
-    console.log('k=%j', result)
     expect(result.length).toEqual(5)
   })
 })
