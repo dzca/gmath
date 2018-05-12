89eb75ba09cf704909403a0aaa39b530b4cbf176
Dustin Zhang <dike.zhang@gmail.com>
2018-05-12 10:58:51 -0400

    deploy.sh run npm install

--- pkg/deploy.sh
+++ pkg/deploy.sh
@@ -7,43 +7,17 @@ APP_NAME=gmath
 APP_ROOT=/app/$APP_NAME
 SYSTEMD_ROOT=/etc/systemd/system
 ##################################################################
-# utility functions begine
+# installation begine
 ##################################################################
 
-_make_dir()
-{
-    directory=$1
-    if [ ! -d $directory ]
-    then
-        echo "making $directory"
-        mkdir -p $directory
-    fi
-}
-
-##################################################################
-# stop application ?
-##################################################################
-
-##################################################################
-# install node application
-##################################################################
-
-_make_dir $APP_ROOT
-
-cp -v gmath.5001.js $APP_ROOT/
-cp -v config.js $APP_ROOT/
-cp -v package.json $APP_ROOT/
-cp -R -v dist $APP_ROOT/
 
 pushd $APP_ROOT
+# install node application
 npm install
-popd
-##################################################################
+
 # install systemd configuration
-##################################################################
-
-cp -v gmath.service $SYSTEMD_ROOT/
-
+# cp -v gmath.service $SYSTEMD_ROOT/
+popd
 ##################################################################
 # start application
 ##################################################################
